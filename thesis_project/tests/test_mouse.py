5b8b5e7175122c1f4c9215904eef125296fa3e90
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-05-22 18:08:37 -0400

    TDD: Adding participant to experiment via Mouse object

--- thesis_project/tests/test_mouse.py
+++ thesis_project/tests/test_mouse.py
@@ -1,7 +1,9 @@
 import unittest
 from models.mouse import Mouse
+from models.experiments import Experiments
 from database import Database
 from data.constants import dbConnection_Krista
+import utilities as util
 
 
 class TestNewMouse(unittest.TestCase):
@@ -34,16 +36,57 @@ class TestNewMouse(unittest.TestCase):
         self.load_mouse = Mouse.from_db(9999)
         self.assertFalse(self.load_mouse.mouse_id is None)
 
+
 class TestAddParticipant(unittest.TestCase):
 
+    test_mouse_table_seed = [(9990, 20200102, 'wild type', 'male', True, 'test experiment one', 20200501, 20200601),
+                             (9991, 20200101, 'wild type', 'male', False, 'test experiment two', 20200515, 20200615),
+                             (9992, 20200101, 'wild type', 'male', True, 'test experiment one', 20200501, 20200601),
+                             (9993, 20200101, 'wild type', 'male', False, 'test experiment two', 20200515, 20200615),
+                             (9994, 20200101, 'wild type', 'female', True, 'test experiment one', 20200501, 20200601),
+                             (9995, 20200101, 'wild type', 'female', False, 'test experiment two', 20200515, 20200615),
+                             (9996, 20200101, 'knock out', 'female', True, 'test experiment one', 20200501, 20200601),
+                             (9997, 20200101, 'knock out', 'female', False, 'test experiment two', 20200515, 20200615),
+                             (9998, 20200101, 'knock out', 'female', True, 'test experiment one', 20200501, 20200601),
+                             (9999, 20200102, 'knock out', 'female', False, 'test experiment two', 20200515, 20200615)]
+    experiment_name_one = 'test experiment one'
+    experiment_name_two = 'test experiment two'
+    experiment_dir_one = '/test/directory/experiment/one'
+    experiment_dir_two = '/test/directory/experiment/two'
+
     def setUp(self):
-        pass
+        Database.initialize(**dbConnection_Krista)
+        self.test_experiment_one = Experiments(self.experiment_name_one, self.experiment_dir_one).save_to_db()
+        self.test_experiment_two = Experiments(self.experiment_name_two, self.experiment_dir_two).save_to_db()
+        self.test_participants_list = []
+        for mouse in self.test_mouse_table_seed:
+            self.test_participants_list.append(Mouse(eartag=mouse[0], birthdate=mouse[1],
+                                                     genotype=mouse[2], sex=mouse[3]).save_to_db())
 
     def tearDown(self):
-        pass
+        for mouse_detail in self.test_mouse_details:
+            mouse_detail.delete_from_db()
+        for mouse in self.test_participants_list:
+            mouse.delete_from_db()
+        self.test_experiment_one.delete_from_db()
+        self.test_experiment_two.delete_from_db()
 
     def test_add_participant(self):
-        pass
+        self.test_mouse_details = []
+        for mouse in self.test_mouse_table_seed:
+            if util.prep_string_for_db(mouse[5]) == util.prep_string_for_db(self.test_experiment_one.experiment_name):
+                self.test_mouse_details.append(Mouse.from_db(mouse[0])
+                                               .add_participant(util.prep_string_for_db(self
+                                                                                        .test_experiment_one
+                                                                                        .experiment_name)))
+            elif util.prep_string_for_db(mouse[5]) == util.prep_string_for_db(self.test_experiment_two.experiment_name):
+                self.test_mouse_details\
+                    .append(Mouse.from_db(mouse[0])
+                            .add_participant(util
+                                             .prep_string_for_db(self
+                                                                 .test_experiment_two.experiment_name)))
+        self.assertListEqual(sorted(self.test_participants_list, key=lambda m: m.eartag),
+                             sorted(Experiments.list_participants(), key=lambda m: m.eartag))
 
 
 if __name__ == '__main__':
