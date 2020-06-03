a3cdd2eeae3ae0ad94184c4ae6836d69065cdab5
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-06-03 18:12:21 -0400

    Refactoring

--- thesis_project/database/handlers/handlers_independent_tables.py
+++ thesis_project/database/handlers/handlers_independent_tables.py
@@ -1,6 +1,6 @@
 from database.create_tables.create_independent_tables import create_mouse_table, create_experiments_table
 from database.cursors import TestingCursor
-from database.handlers.handlers_participant_details import create_participant_details_table
+import database.create_tables.create_participant_details as create_pd
 
 
 def handler_create_all_empty_tables(postgresql):
@@ -8,4 +8,5 @@ def handler_create_all_empty_tables(postgresql):
         cursor.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
         create_mouse_table(cursor)
         create_experiments_table(cursor)
-        create_participant_details_table(cursor)
+        create_pd.create_participant_details_table(cursor)
+        create_pd.create_view_all_participants_all_experiments(cursor)
