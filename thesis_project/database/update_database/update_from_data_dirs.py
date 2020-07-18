df2bcd15d5bd5d60c9d029f4300ddd4969ae07b0
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-07-18 19:45:03 -0400

    Completed update from data dirs. Note: it takes about 15 minutes to completed create the database and insert all data at this point.

--- thesis_project/database/update_database/update_from_data_dirs.py
+++ thesis_project/database/update_database/update_from_data_dirs.py
@@ -1,6 +1,11 @@
 from data.constants import sharedx_prefix
 from pathlib import Path
+
+from models.mouse import Mouse
 from models.participant_details import ParticipantDetails
+from models.sessions import Session
+from models.folders import Folder
+from models.trials import Trial
 
 
 def update_from_data_dirs(experiment):
@@ -9,8 +14,31 @@ def update_from_data_dirs(experiment):
         return False
 
     all_participant_dirs = list(Path(experiment.experiment_dir).glob('et*/'))
-    # in_db = ParticipantDetails.list_participants(experiment.experiment_name)
 
     for participant_dir in all_participant_dirs:
-        eartag_num = participant_dir.name.strip('et')
-        print(eartag_num)
+
+        eartag_num = int(participant_dir.name.strip('et'))
+        mouse = Mouse.from_db(eartag_num)
+
+        if mouse is None:
+            continue
+
+        mouse_details = ParticipantDetails.from_db(eartag_num, experiment.experiment_name)
+
+        if mouse_details is None:
+            continue
+
+        training_dir = Path(mouse_details.participant_dir).joinpath('Training')
+        all_session_dirs = training_dir.glob(f'et{eartag_num}_*_*T*/')
+        for session_dir in all_session_dirs:
+            session_date = int(session_dir.name.split('_')[1])
+            session = Session(mouse.mouse_id, experiment.experiment_id, str(session_dir), session_date).save_to_db()
+            all_folder_dirs = session_dir.glob('Reaches*')
+            for folder_dir in all_folder_dirs:
+                folder = Folder(session.session_id, str(folder_dir)).save_to_db()
+                all_trial_dirs = folder_dir.glob('*R*.mp4')
+                for trial_dir in all_trial_dirs:
+                    Trial(experiment.experiment_id,
+                          folder.folder_id,
+                          str(trial_dir),
+                          session.session_date).save_to_db()
