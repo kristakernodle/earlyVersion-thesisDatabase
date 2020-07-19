08dc6340e0956b9d3c29888c05c12068145afe7f
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-07-18 22:26:51 -0400

    Need to add functionality: models.trials.list_trials_for_folder(folder_id)

--- thesis_project/database/update_database/script_updated_blinded_data_from_csv_key.py
+++ thesis_project/database/update_database/script_updated_blinded_data_from_csv_key.py
@@ -4,6 +4,7 @@ import csv
 from models.folders import Folder
 from models.reviewers import Reviewer
 from models.blind_folders import BlindFolder
+import models.trials
 
 scrambled_mask_keys_file = Path('/Users/Krista/Desktop/master_file_keys_scrambled.csv')
 with open(scrambled_mask_keys_file) as f:
@@ -16,3 +17,4 @@ for (LED_detection_output_csv, reviewer_fullname, blind_name) in scrambled_mask_
     folder = Folder.from_db(str(folder_dir))
     reviewer = Reviewer.from_db(reviewer_fullname)
     print(BlindFolder(folder.folder_id, reviewer.reviewer_id, blind_name))
+    all_trials = models.trials.list_trials_for_folder(folder.folder_id)
