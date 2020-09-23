ba2a51bd20dfd208c459f8ee695070de7daddf1e
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-09-23 10:39:02 -0400

    Cleaning up, correcting code to allow for database updates.

---
+++ thesis_project/main.py
@@ -0,0 +1,32 @@
+import blind_review
+import project_config
+import utilities as util
+import thesis_database_pkg
+
+project_config_folder_path = project_config.__path__._path
+
+
+def get_database_config(project_name):
+    project_config = f"{project_config_folder_path[0]}/{project_name}_project_config.yaml"
+    return util.read_project_config(project_config)
+
+
+def update_project_database(project_name):
+    database_config_path = get_database_config(project_name)
+    db_details, _, main_user, _ = thesis_database_pkg.utilities.read_config(database_config_path)
+    thesis_database_pkg.populate_from_files.populate_db_from_back_up_csv(db_details, main_user)
+    thesis_database_pkg.update_from_data_dirs(db_details, main_user)
+
+
+def main():
+    project_name = input("Project name: ")
+    update = input(f"Do you want to update the database for {project_name}? [y/N]: ")
+
+    if update.lower() in ['y', 'yes', 't', 'true', '1']:
+        update_project_database(project_name)
+    else:
+        print("Project not updated")
+
+
+if __name__ == '__main__':
+    main()
--- thesis_project/utilities.py
+++ thesis_project/utilities.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-def read_config(yaml_full_path):
+def read_project_config(yaml_full_path):
     with open(yaml_full_path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data['database_config_dir']
