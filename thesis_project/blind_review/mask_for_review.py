adc39e31d6e968bb14c98a7354eea1fc1559b485
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-09-24 16:47:17 -0400

    added search queries and some notebooks, got a "lost" blind_folder function going in the sketches nb

--- thesis_project/blind_review/mask_for_review.py
+++ thesis_project/blind_review/mask_for_review.py
@@ -1,10 +1,21 @@
 import thesis_database.tools.get as get
-import blind_review.utilities as util
-from thesis_database import BlindTrial, BlindFolder, Trial, Experiment
-import thesis_database
+import string
+from thesis_database import BlindTrial, BlindFolder, Trial, Experiment, Reviewer, Folder, initialize_database
 import shutil
+import random
 from shutil import Error
 from pathlib import Path
+
+
+def random_string_generator(len_string=10):
+    """Generates a random string of length len_string.
+    String will contain only lowercase letters and digits.
+    :param len_string: length of returned string (default 10)
+    :return: string of length len_string
+    """
+
+    lowercase_letters_and_digits = list(string.ascii_lowercase + string.digits)
+    return ''.join(random.choices(lowercase_letters_and_digits, weights=None, k=len_string))
 
 
 def mask_trial(trial, masked_trial_dir):
@@ -23,13 +34,14 @@ def mask_folder(reviewer, folder):
 
     if masked_folder is None:
         # Generate a unique blind name
-        blind_name = util.random_string_generator(10)
+        blind_name = random_string_generator(10)
         while blind_name in get.list_all_blind_names():
-            blind_name = util.random_string_generator(10)
+            blind_name = random_string_generator(10)
         BlindFolder(folder.folder_id, reviewer.reviewer_id, blind_name).save_to_db()
         masked_folder = BlindFolder.from_db(reviewer_id=reviewer.reviewer_id, folder_id=folder.folder_id)
 
     masked_folder_dir = Path(reviewer.toScore_dir).joinpath(masked_folder.blind_name)
+    masked_folder_dir.mkdir()
 
     for idx, trial_id in enumerate(set(get.list_trial_ids_for_folder(folder))):
         trial_num = str(idx + 1)
@@ -43,6 +55,7 @@ def mask_folder(reviewer, folder):
             break
 
 
+# TODO move this to the correct place
 def list_all_folders_not_blinded(experiment):
     # List all folder_ids AND all folder_ids associated with masked folders
     all_folder_ids = set(get.list_folder_ids_for_experiment(experiment))
@@ -52,4 +65,22 @@ def list_all_folders_not_blinded(experiment):
     all_folder_ids_not_blinded = all_folder_ids.difference(all_blinded_folders_folder_ids)
 
     # Load each folder as an object into a list for return
-    return [thesis_database.Folder.from_db(folder_id=folder_id) for folder_id in all_folder_ids_not_blinded]
+    return [Folder.from_db(folder_id=folder_id) for folder_id in all_folder_ids_not_blinded]
+
+
+if __name__ == '__main__':
+    project_name = 'hello'
+    experiment_name = 'skilled-reaching'
+    reviewer_name = 'Krista K'
+    num_files = 5
+
+    initialize_database(project_name)
+    the_experiment = Experiment.from_db(experiment_name=experiment_name)
+    the_reviewer = Reviewer.from_db(reviewer_fullname=reviewer_name)
+
+    all_folders_not_blinded = list_all_folders_not_blinded(the_experiment)
+    folders_to_mask = random.sample(all_folders_not_blinded, num_files)
+    print("Beginning to mask")
+    for the_folder in folders_to_mask:
+        print(the_folder.folder_dir)
+        mask_folder(the_reviewer, the_folder)
